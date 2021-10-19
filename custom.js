82af2a7dd9892dd306ec85440942b92a12859e7f
mijeong96 <alwjdekdma2@gmail.com>
2021-10-19 17:23:25 +0900

    5

--- custom.js
+++ custom.js
@@ -9,10 +9,24 @@ btnCall.onclick = function(e){
     menuMo.classList.toggle("on"); 
 }
 
+//마우스 호버시 2depth 활성화
 $("#gnb>li").on("mouseenter", function(){
     $(this).find(".sub").show();
 });
 
 $("#gnb>li").on("mouseleave", function(){
     $(this).find(".sub").hide();
+});
+
+//포커스 이동시 2depth 활성화
+//.each() - 배열반복문
+$("#gnb>li").each(function(index){
+
+    $("#gnb>li").eq(index).find("a").on("focusin",function(){
+        $("#gnb>li").eq(index).find(".sub").show();
+    });
+
+    $("#gnb>li").eq(index).find("a").last().on("focusout",function(){
+        $("#gnb>li").eq(index).find(".sub").hide();
+    });
 });
